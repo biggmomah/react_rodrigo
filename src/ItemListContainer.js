3d7b98529b721679ad6d1334b521fa4bbf3171a3
Rodrigo <rrodrigo2@live.com>
2021-12-17 12:25:42 -0300

    rompiendo el codigo

--- src/ItemListContainer.js
+++ src/ItemListContainer.js
@@ -1,11 +1,26 @@
 function ItemListCointainer({titulo}){
+    let contador= 0
 
-     const{nombre, precio} = titulo
+    const aumentadorContador=()=>{
+        console.log('aumentado')
+        contador++
+        console.log(contador)
+    }
+    const{nombre, precio} = titulo
      
     return(
-        <h3>{nombre}
-            <p>{precio}</p>
-        </h3>
+        <div>
+            <h3>{nombre}
+                <p>{precio}</p>
+            </h3>
+
+            <div>
+                <p>Contador actual: {contador}</p>
+                <button onClick={aumentadorContador}>aumentar</button>
+            </div>
+        </div>
+
+        
     )
 }
 
