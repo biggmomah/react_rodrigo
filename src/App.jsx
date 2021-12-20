924bc699d4ffd8c969d7fdbe9661781b061b8679
Rodrigo <rrodrigo2@live.com>
2021-12-20 14:17:20 -0300

    proyecto contador con boton

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App(){
      <Navbar/>
      <ItemListContainer titulo={dato}/>
      <ItemCount stock={5} initial={1} />
+     
     </>
     )
 }
--- src/ItemCount.jsx
+++ src/ItemCount.jsx
@@ -1,27 +1,30 @@
 import {useState} from 'react';
 
-function ItemCount({ stock , initial , onAdd }){
+const ItemCount= ({ stock , initial , onAdd }) => {
     
-    const [contador, setContador]=useState(0)
-
-    const aumentadorContador=()=>{
-        setContador(contador +1)
-        console.log('aumenta')
+    const [contador, setContador]=useState(initial)
+    const aumentarContador=()=>{
+        if ( contador <= stock){
+            setContador(contador +1)
+        }console.log('excediste cantidad stock')
     }
 
-    const disminuirContador=()=>{
-        setContador(contador -1)
+    const disminuirContador=()=>{   
+        if(contador > initial){
+            setContador(contador -1)
+        }console.log('no es posible seguir restando unidades')
     }
 
     const resetearContador=()=>{
-        setContador(0)
+        setContador(initial)
+        console.log('reiniciado')
     }
 
     return(
         <div>
-            <button type="button" onCLick={aumentadorContador} className="btn btn-outline-primary btn-sm">Agregar</button>
-            <button type="button" onCLick={disminuirContador} className="btn btn-outline-secondary btn-sm">Restar</button>
-            <button type="button" onCLick={resetearContador} className="btn btn-danger btn-sm">Reiniciar</button>
+            <button type="button" onClick={aumentarContador} className="btn btn-outline-primary btn-sm">Agregar</button>
+            <button type="button" onClick={disminuirContador} className="btn btn-outline-secondary btn-sm">Restar</button>
+            <button type="button" onClick={resetearContador} className="btn btn-danger btn-sm">Reiniciar</button>
             <p>Contador actual: {contador}</p>
         </div>
     )
